fa1885bb0b4dc7ebeadc94db7079bd06fe54f9bf
Ethan Masching <45413287+2manyflags@users.noreply.github.com>
2018-11-27 22:16:04 -0600

    Initial commit

---
+++ pacman/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pacman
+{
+    class Program
+    {
+        static void Main()
+        {
+            char [,] map = new char[20,30];
+            int counter=0;
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\DSU\\source\\repos\\map.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    map[counter,0] = line.Split(',').ToArray();
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            finally
+            {
+                Console.WriteLine("File read successfully");
+            }
+            Console.Read();
+        }
+    }
+}
